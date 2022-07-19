43ebb6b97e12045e7398b11c187317f7f58343ac
Renato <rrschiavofilho@gmail.com>
2022-07-18 21:41:27 -0300

    initial implementation

---
+++ lib/guess.ex
@@ -0,0 +1,69 @@
+defmodule Guess do
+  use Application
+
+  def start(_,_) do
+    run()
+    {:ok, self()}
+  end
+
+  def run() do
+    IO.puts("Lets's play Guess the Number")
+
+    IO.gets("Pick a difficult level (1, 2 or 3): ")
+    |> parse_input
+    |> pickup_number
+    |> play
+    |> IO.inspect
+  end
+
+  def play(picked_num) do
+    IO.gets("I have my number. What is your guess?: ")
+    |> parse_input
+    |> guess(picked_num, 1)
+  end
+
+  def guess(user_guess, picked_num, count) when user_guess > picked_num do
+    IO.gets("Too high. Guess again: ")
+    |> parse_input
+    |> guess(picked_num, count + 1)
+  end
+
+  def guess(user_guess, picked_num, count) when user_guess < picked_num do
+    IO.gets("Too low. Guess again: ")
+    |> parse_input
+    |> guess(picked_num, count + 1)
+  end
+
+  def guess(_user_guess, _picked_num, count) do
+    IO.puts("You got it!! #{count} guess")
+  end
+
+  def pickup_number(level) do
+    level
+    |> get_range
+    |> Enum.random
+  end
+
+  def parse_input(:error) do
+    IO.puts("Invalid input!!!")
+    run()
+  end
+
+  def parse_input({num, _}), do: num
+
+  def parse_input(data) do
+    data
+    |> Integer.parse
+    |> parse_input
+  end
+
+  def get_range(level) do
+    case level do
+      1 -> 1..10
+      2 -> 1..100
+      3 -> 1..1000
+      _ -> IO.puts("Invalid level!!!")
+        run()
+    end
+  end
+end
