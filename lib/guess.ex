567481f5c10a17959e6b0b6d3fe487e4cdde33e0
Renato <rrschiavofilho@gmail.com>
2022-07-19 21:18:28 -0300

    scoreboard

--- lib/guess.ex
+++ lib/guess.ex
@@ -10,38 +10,52 @@ defmodule Guess do
     IO.puts("Lets's play Guess the Number")
 
     IO.gets("Pick a difficult level (1, 2 or 3): ")
-    |> parse_input
-    |> pickup_number
-    |> play
-    |> IO.inspect
+    |> parse_input()
+    |> pickup_number()
+    |> play()
   end
 
   def play(picked_num) do
     IO.gets("I have my number. What is your guess?: ")
-    |> parse_input
+    |> parse_input()
     |> guess(picked_num, 1)
   end
 
   def guess(user_guess, picked_num, count) when user_guess > picked_num do
     IO.gets("Too high. Guess again: ")
-    |> parse_input
+    |> parse_input()
     |> guess(picked_num, count + 1)
   end
 
   def guess(user_guess, picked_num, count) when user_guess < picked_num do
     IO.gets("Too low. Guess again: ")
-    |> parse_input
+    |> parse_input()
     |> guess(picked_num, count + 1)
   end
 
   def guess(_user_guess, _picked_num, count) do
     IO.puts("You got it!! #{count} guess")
+    show_score(count)
   end
 
   def pickup_number(level) do
     level
-    |> get_range
+    |> get_range()
     |> Enum.random
+  end
+
+  def show_score(guesses) when guesses > 6 do
+    IO.puts("Better luck next time")
+  end
+
+  def show_score(guesses) do
+    {_, msg} = %{1..1 => "You're a mind rider!",
+      2..4 => "Most impresive",
+      3..6 => "You can do better than that"}
+    |> Enum.find(fn {range, _} ->
+      Enum.member?(range, guesses)
+    end)
+    IO.puts(msg)
   end
 
   def parse_input(:error) do
@@ -53,8 +67,8 @@ defmodule Guess do
 
   def parse_input(data) do
     data
-    |> Integer.parse
-    |> parse_input
+    |> Integer.parse()
+    |> parse_input()
   end
 
   def get_range(level) do
